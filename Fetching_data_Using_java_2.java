90c5f7f437994c0665e36be983c98d8c86811687
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-02-28 00:18:42 +0530

    Add files via upload

---
+++ Fetching_data_Using_java_2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class Main {
+    public static void main(String[] args)  throws Exception{
+        String url="https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=2fe1fbed5bc347d2b36d319be4a8fd7d";
+        HttpRequest request=HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+        HttpClient httpClient=HttpClient.newBuilder().build();
+        HttpResponse<String> httpResponse =httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.statusCode());
+        System.out.println(httpResponse.body());
+
+    }
+}
